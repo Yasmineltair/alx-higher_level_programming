086d343fcf9f99d5ea11d16a829cd2b822ca1377
Yasmineltair <yasmineltair293@gmail.com>
2023-10-31 10:51:38 -0700

    1-last_digit.py

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -9,5 +9,5 @@ if tens > 5:
    print(f"Last digit of {number:d} is {tens:d} and is greater than 5")
 elif tens == 0:
    print(f"Last digit of {number:d} is {tens:d} and is 0")
-elif tens < 6:
+   else :
    print(f"Last digit of {number:d} is {tens:d} and is less than 6 and not 0")
