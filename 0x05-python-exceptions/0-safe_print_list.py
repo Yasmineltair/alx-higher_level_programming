60b6b5c9ce4f75fbb8d6c4b97d5cc205c3db2b78
Yasmineltair <yasmineltair293@gmail.com>
2023-11-20 11:10:40 -0800

    new

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -6,7 +6,6 @@ def safe_print_list(my_list=[], x=0):
         try:
             print("{}".format(my_list[i]), end="")
             printd += 1
-        except:
-            continue
-    print()
+        except Exception:
+            None
     return printd
