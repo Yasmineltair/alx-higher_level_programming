44cf9e635d83ba53845cf47ced74f3ac41604331
Yasmineltair <yasmineltair293@gmail.com>
2023-11-21 12:29:13 -0800

    new

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""Defines a class square."""
+
+
+class Square:
+    """Defines a square by: (based on 0-square.py)."""
+    def size(self, size):
+        """constructor.
+        Args:
+        size: size of the square."""
+        self.__size = size
