9893eda01488d2e69f77f87aef7021e66636560e
Yasmineltair <yasmineltair293@gmail.com>
2023-12-04 14:45:09 -0800

    tests

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,60 @@
+#!/usr/bin/python3
+""" module contains class square"""
+
+
+class BaseGeometry:
+    """ class BaseGeometry"""
+
+    def area(self):
+        """ public instance"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ public instance method """
+
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """ class Rectangle """
+
+    def __init__(self, width, height):
+        """ instante method """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """ area method """
+
+        return self.__width * self.__height
+
+    def __str__(self):
+        """ str method """
+
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
+
+
+class Square(Rectangle):
+    """ square class"""
+
+    def __init__(self, size):
+        """ initialization"""
+
+        super().__init__(size, size)
+        self.integer_validator("size", size)
+        self.__size = size
+
+    def area(self):
+        "area"
+
+        return self.__size ** 2
+
+    def __str__(self):
+        """ str method """
+
+        return "[Square] {}/{}".format(self.__size, self.__size)
