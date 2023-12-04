7a312a20fc9ae8ad9edcd8daa06d52cd26a22770
Yasmineltair <yasmineltair293@gmail.com>
2023-12-04 14:35:54 -0800

    tests

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,6 +1,42 @@
 #!/usr/bin/python3
-Rectangle = __import__('9-rectangle').Rectangle
 """ module contains class square"""
+
+
+class BaseGeometry:
+    """ class BaseGeometry"""
+
+    def area(self):
+        """ public instance"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ public instance method """
+
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
+
+
+class Rectangle(BaseGeometry):
+    """ class Rectangle """
+
+    def __init__(self, width, height):
+        """ instante method """
+        self.integer_validator("width", width)
+        self.__width = width
+        self.integer_validator("height", height)
+        self.__height = height
+
+    def area(self):
+        """ area method """
+
+        return self.__width * self.__height
+
+    def __str__(self):
+        """ str method """
+
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
 
 
 class Square(Rectangle):
@@ -9,7 +45,7 @@ class Square(Rectangle):
     def __init__(self, size):
         """ initialization"""
 
-        super().__init__(size, size)
+        super().__init(size, size)
         self.integer_validator("size", size)
         self.__size = size
 
