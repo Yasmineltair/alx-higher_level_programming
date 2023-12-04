f03ccca153bafe7e3a081ad1dfc89c294a24c520
Yasmineltair <yasmineltair293@gmail.com>
2023-12-04 15:04:42 -0800

    test

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 """ module with class list """
 
+
 class MyList(list):
     """ class list inherted frmo list"""
     pass
 
     def print_sorted(self):
         """ Public instance method """
-        print (sorted(list(self)))
+        print(sorted(list(self)))
