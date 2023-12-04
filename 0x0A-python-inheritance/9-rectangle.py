0f8c33618517fee6b23fdd26a8738a781b3a3887
Yasmineltair <yasmineltair293@gmail.com>
2023-12-04 13:34:32 -0800

    up

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('8-base_geometry').BaseGeometry
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """ module contains class BaseGeometry"""
 
 
