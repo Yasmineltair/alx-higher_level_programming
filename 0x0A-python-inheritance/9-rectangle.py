ca48657412076685a6d71b8a93c2849c7ac61383
Yasmineltair <yasmineltair293@gmail.com>
2023-12-04 14:31:22 -0800

    test

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,6 +1,21 @@
 #!/usr/bin/python3
-BaseGeometry = __import__('7-base_geometry').BaseGeometry
 """ module contains class BaseGeometry"""
+
+
+class BaseGeometry:
+    """ class BaseGeometry"""
+
+    def area(self):
+        """ public instance"""
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """ public instance method """
+
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
 
 
 class Rectangle(BaseGeometry):
