08198103f10b24e37238956fdc0002bdbc6f3010
Yasmineltair <yasmineltair293@gmail.com>
2023-12-05 08:50:53 -0800

    4-from_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-
+""" module contains json"""
 import json
-
+""" import json"""
 
 def to_json_string(my_obj):
     """function that returns he JSON representation of an object(string)"""
