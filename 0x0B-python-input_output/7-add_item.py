473e66ec4dffafe6c00701529ec89beeef9fb6a0
Yasmineltair <yasmineltair293@gmail.com>
2023-12-05 12:00:14 -0800

    7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -2,9 +2,13 @@
 """ contain json function"""
 
 import json
+""" import json"""
 import os.path
+""" import os.path"""
 import sys
+""" import sys"""
 from sys import argv
+""" import argv"""
 
 save_to_json_file = __import__(5-save_to_json_file).save_to_json_file
 load_from_json_file = __import__(6-load_from_json_file).load_from_json_file
