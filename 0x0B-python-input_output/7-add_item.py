fc3bad34d5594bd0f4f2ab39faaa8f5de2a835ee
Yasmineltair <yasmineltair293@gmail.com>
2023-12-05 11:58:26 -0800

    7-add_item.py

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+""" contain json function"""
+
+import json
+import os.path
+import sys
+from sys import argv
+
+save_to_json_file = __import__(5-save_to_json_file).save_to_json_file
+load_from_json_file = __import__(6-load_from_json_file).load_from_json_file
+
+filename = "add_item.json"
+json_list = []
+
+if os.path.exits(filename):
+    json_list = load_from_json_file(filename)
+
+for i in argv[1:]:
+    json_list.append(i)
+
+save_to_json_file(json_list, filename)
