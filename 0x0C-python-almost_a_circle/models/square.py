8b6264f0675b7b3eeb1d6bd8c709aeb7f4013221
Yasmineltair <yasmineltair293@gmail.com>
2023-12-11 09:48:01 -0800

    10

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" module square """
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """ class Square that inherts frmo Rectangle"""
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ Class constructor """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """ size getter"""
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """ size setter"""
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.width = value
+            self.height = value
