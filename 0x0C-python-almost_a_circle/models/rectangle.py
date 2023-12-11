f70297a234c29988f4b3c3b0efc333b364e686eb
Yasmineltair <yasmineltair293@gmail.com>
2023-12-11 07:22:08 -0800

    rectangle.py

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -14,3 +14,63 @@ class Rectangle(Base):
         self.height = height
         self.x = x
         self.y = y
+
+    @width.getter
+    def width(self):
+        """ width getter"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ width setter """
+        if type(value) is not int:
+            raise TypeError(f"{value} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{value} must be > 0")
+        else:
+            self.__width = value
+
+    @height.getter
+    def height(self):
+        """ height getter"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ height setter """
+        if type(value) is not int:
+            raise TypeError(f"{value} must be an integer")
+        elif value <= 0:
+            raise ValueError(f"{value} must be > 0")
+        else:
+            self.__height = value
+
+    @x.getter
+    def x(self):
+        """ x getter"""
+        return self.__x
+
+    @x.setter
+    def x(self, value):
+        """ x setter """
+        if type(value) is not int:
+            raise TypeError(f"{value} must be an integer")
+        elif value < 0:
+            raise ValueError(f"{value} must be >= 0")
+        else:
+            self.__x = value
+
+    @y.getter
+    def y(self):
+        """ y getter"""
+        return self.__y
+
+    @y.setter
+    def y(self, value):
+        """ y setter """
+        if type(value) is not int:
+            raise TypeError(f"{value} must be an integer")
+        elif value < 0:
+            raise ValueError(f"{value} must be >= 0")
+        else:
+            self.__y = value
