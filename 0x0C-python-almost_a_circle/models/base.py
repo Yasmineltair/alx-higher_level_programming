26ab2181f1b1414eb321b9bd9921ea3e96a0c430
Yasmineltair <yasmineltair293@gmail.com>
2023-12-11 11:34:09 -0800

    base

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -50,5 +50,5 @@ class Base:
             dummy = cls(1, 1)
         elif cls.__name__ == "Square":
             dummy = cls(1)
-            dummy.update(**dictionary)
-            return dummy
+        dummy.update(**dictionary)
+        return dummy
