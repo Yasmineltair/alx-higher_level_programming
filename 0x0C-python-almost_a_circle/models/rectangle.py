b81c8e65d99838bea5427a1d000048ab71f0baa1
Yasmineltair <yasmineltair293@gmail.com>
2023-12-11 08:19:11 -0800

    4

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -73,3 +73,7 @@ class Rectangle(Base):
             raise ValueError("y must be >= 0")
         else:
             self.__y = value
+
+    def area(self):
+        """ area method """
+        return self.__width * self.height
