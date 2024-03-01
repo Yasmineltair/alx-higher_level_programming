e61fb0a69e21da4fd9efda4b4b67b3bd2ff94be5
Yasmineltair <yasmineltair293@gmail.com>
2024-03-01 14:15:21 -0800

    100

--- 0x11-python-network_1/100-github_commits.py
+++ 0x11-python-network_1/100-github_commits.py
@@ -4,19 +4,16 @@ import sys
 import requests
 
 
-def main():
-    """func doc"""
-    owner = sys.argv[1]
-    repo = sys.argv[2]
-    limit = 10
-    url = 'https://api.github.com/repos\
-/{repo}/{owner}/commits?per_page={limit}'
-
-    response = requests.get(url).json()
-    for commit in response:
-        name = commit.get("commit").get("author").get("name")
-        print(f'{commit.get("sha")}: {name}')
-
-
 if __name__ == "__main__":
-    main()
+    url = "https://api.github.com/repos/{}/{}/commits".format(
+        sys.argv[2], sys.argv[1])
+
+    r = requests.get(url)
+    commits = r.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
