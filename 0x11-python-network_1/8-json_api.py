225192166ed4f7d30e6a364af97fb4bd47e4ac51
Yasmineltair <yasmineltair293@gmail.com>
2024-03-01 14:22:42 -0800

    8

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -5,18 +5,15 @@ import sys
 
 
 if __name__ == "__main__":
-    if len(sys.argv) > 1:
-        q = sys.argv[1]
-    else:
-        q = ""
-        url = "http://0.0.0.0:5000/search_user"
-        params = {"q": q}
-        result = requests.post(url, data=params)
-        try:
-            resjson = result.json()
-            if resjson:
-                print(f'[{resjson["id"]}] {resjson["name"]}')
-            else:
-                print("No result")
-        except Exception:
+    letter = "" if len(sys.argv) == 1 else sys.argv[1]
+    pyload = {"q": letter}
+ 
+    result = requests.post("http://0.0.0.0:5000/search_user", data=pyload)
+    try:
+        response = result.json()
+        if response == {}:
+            print("No result")
+	else:
+	    print ("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
             print("Not a valid JSON")
