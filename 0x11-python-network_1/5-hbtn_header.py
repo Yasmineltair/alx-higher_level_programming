51d61b684c44ae11160eba67fe6e7cfe1164a328
Yasmineltair <yasmineltair293@gmail.com>
2024-03-01 13:04:28 -0800

    5-hbtn_header.py

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" module documentation"""
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    url = sys.argv[1]
+    result = requests.get(url)
+    print(result.headers.get("X=Request-Id"))
