070941af2ed131e53012a8fd5aea0d423cdbf2ab
Yasmineltair <yasmineltair293@gmail.com>
2024-03-26 13:10:23 -0700

    0-readme.js

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+
+const fs = require('fs');
+
+fs.readFile(process.argv[2], 'utf8', function (error, content) {
+
+  if (error) {
+    console.log(error);
+  } else {
+    process.stdout.write(content);
+  }
+});
